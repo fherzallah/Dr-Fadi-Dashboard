a6d86b48feb23a811ef038c81f2d147a092eca5c
152200 <akhmad.badran@gmail.com>
2024-12-14 17:19:02 +0200

    forth commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import NavBar from './main-page/NavBar'
-import About from './main-page/About'
-import MainPublications from './main-page/main-publicatoins'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import AllPublications from './publications-page/AllPublications'
-import MainPage from './main-page/MainPage'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import NavBar from './main-page/NavBar';
+import AllPublications from './publications-page/AllPublications';
+import MainPage from './main-page/MainPage';
+
+const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: (
+        <>
+          <NavBar />
+          <MainPage />
+        </>
+      ),
+    },
+    {
+      path: '/publications',
+      element: (
+        <>
+          <NavBar />
+          <AllPublications />
+        </>
+      ),
+    },
+  ],
+  { basename: '/Dr-Fadi-Dashboard' } // Set your base path here
+);
+
 function App() {
-  
-  return (
-    <div>
-      
-      <Router>
-      <NavBar/>
-        <Routes>
-          <Route path='/publications' element={<AllPublications/>}></Route>
-          <Route path='/' element={<MainPage/>}></Route>
-        </Routes>
-      </Router>
-      {/* <div style={{height:"4000px"}}></div> */}
-    </div>
-  )
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
